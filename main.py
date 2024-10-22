505a8425f44986b037840b561992f730b4c97d55
Felipe <nemmanc@protonmail.com>
2024-10-22 15:25:26 -0300

    add: basic-ping-test

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ async def on_ready():
     print("==== Bot is ready ====")
     print("v0.1")
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("olá")
+@bot.command(aliases=["Ping", "Pping", "PING", ""])
+async def ping(ctx):
+    ping_embed = discord.Embed(title="Ping test", description="Latency", color=discord.Color.green())
+    ping_embed.add_field(name="Bot latency (ms): ", value=f"{round(bot.latency * 1000)}ms.", inline=True)
+    ping_embed.set_footer(text=f"Requested: {ctx.author.name}", icon_url=ctx.author.avatar)
+    await ctx.send(embed=ping_embed)
 
 with open("token.txt") as file:
     token = file.read()
