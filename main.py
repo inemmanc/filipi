a23177c3dc2452f3ef16872a31cf25f284068f2d
Felipe <nemmanc@protonmail.com>
2024-10-22 14:28:13 -0300

    add: new-prefix && add: hello-test-command

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="filipi ", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("==== Bot is ready ====")
     print("v0.1")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("olá")
 
 with open("token.txt") as file:
     token = file.read()
