6a40fc5ffd759e7b5387b6572a3b19b995ef2796
Felipe <nemmanc@protonmail.com>
2024-10-22 14:10:17 -0300

    add: bot-on-ready-command

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-bot.run("")
+@bot.event
+async def on_ready():
+    print("==== Bot is ready ====")
+
+bot.run("MTI5ODA5Mjg5MzIxMDQxNTExNA.GmwVr1.HEVSJI4YNdGRWC9cdYtPlclQym6LN2JBApjIk0")
