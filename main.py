93948e46d2a149775a234d9d04cf2e208b48ec9d
Felipe <nemmanc@protonmail.com>
2024-10-22 14:14:27 -0300

    add: token-file-reader

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
 @bot.event
 async def on_ready():
     print("==== Bot is ready ====")
     print("v0.1")
 
-bot.run("MTI5ODA5Mjg5MzIxMDQxNTExNA.GmwVr1.HEVSJI4YNdGRWC9cdYtPlclQym6LN2JBApjIk0")
+with open("token.txt") as file:
+    token = file.read()
+
+bot.run("")
