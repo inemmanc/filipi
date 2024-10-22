968502bd2a532252b3c17c69d3958265bf8bac15
Felipe <nemmanc@protonmail.com>
2024-10-22 14:11:30 -0300

    add: version-print

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print("==== Bot is ready ====")
+    print("v0.1")
 
 bot.run("MTI5ODA5Mjg5MzIxMDQxNTExNA.GmwVr1.HEVSJI4YNdGRWC9cdYtPlclQym6LN2JBApjIk0")
