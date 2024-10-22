7b92b138ebd3262469a3037b866cc1ac5f6f8b48
Felipe <nemmanc@protonmail.com>
2024-10-22 15:31:11 -0300

    add: cogs-pre-req

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+import os
+import asyncio
+
 bot = commands.Bot(command_prefix="filipi ", intents=discord.Intents.all())
 
 @bot.event
@@ -17,5 +20,10 @@ async def ping(ctx):
 
 with open("token.txt") as file:
     token = file.read()
+
+async def Load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[: -3]}")
 
 bot.run(token)
